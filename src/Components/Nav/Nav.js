60875c9f61ed1544f5b78bb4320e9d9483e628e3
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-15 15:00:57 -0600

    added nav links

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../Nav/Nav.css'
 
 function Nav() {
   return (
     <main className='Nav'>
-        
+        <nav role="navigation" className="nav-top">
+        <a className='about-link' href="#About">About</a>
+        <a className='project-link' href="#Projects">Projects</a>
+        <a className='contact-link' href="#Contact">Contact</a>
+        {/* <a className='resume-link' href={resume} target="_blank">Resume</a> */}
+    </nav>
     </main>
   );
 }
