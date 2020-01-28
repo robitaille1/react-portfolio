1e32138748467fbb035b215047f842b074cadc8b
Lucas Robitaille <lucrobitaille@ymail.com>
2020-01-28 15:57:09 -0600

    created Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Header/Header.css'
+
+function Nav() {
+  return (
+    <main className='Header'>
+        
+        
+    </main>
+  );
+}
+
+export default Nav;
