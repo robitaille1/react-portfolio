ff769195e152ee80790858e71675fca73e83daa2
Lucas Robitaille <lucrobitaille@ymail.com>
2020-02-11 12:29:06 -0600

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './Components/Nav/Nav'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <main className='App'>
       <Nav />
+      <Header />
     </main>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../Header/Header.css'
 
-function Nav() {
-  return (
+const header = () => (
     <main className='Header'>
-        
-        
+        <header className="head" id="About">
+            <div>
+                <h1>Lucas Robitaille</h1>
+                <p className="tagline">Fullstack Developer <span className="accent">|</span> Austin, Texas</p>
+            </div>
+        </header>
     </main>
-  );
-}
+);
 
-export default Nav;
+
+export default header;
