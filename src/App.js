25916101b5ef4db8bc9e19ae753959d70914981a
Lucas Robitaille <lucrobitaille@ymail.com>
2020-02-12 19:56:12 -0600

    added minor styling changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './Components/Nav/Nav'
 import Header from './Components/Header/Header'
+import Bio from './Components/Bio/Bio'
 
 function App() {
   return (
     <main className='App'>
       <Nav />
       <Header />
+      <Bio />
     </main>
   );
 }
