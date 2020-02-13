b9de459fb695935fbebf658edcfae575fe534cfd
Lucas Robitaille <lucrobitaille@ymail.com>
2020-02-12 19:55:56 -0600

    added Bio component

---
+++ src/Components/Bio/Bio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Bio/Bio.css'
+
+const bio = () => (
+    <main className='bio'>
+        <p>
+            I am a creative, problem solving web developer. I love working in teams and contributing to projects that I feel passionate about. Creating dynamic, responsive, and interactive web design is what I do best.
+        </p>
+        <p>
+            I also love sports (Go Chelsea!), tea, and traveling to experience different cultures through food.
+        </p>
+    </main>
+);
+
+
+export default bio;
