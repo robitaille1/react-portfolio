fe63da74724826e5fe9763949d15f0c3a9b7ad50
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-24 17:04:49 -0500

    added Tech component and basic styling

---
+++ src/Components/Tech/Tech.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Tech.scss";
+
+const tech = () => (
+  <section className="tech">
+    <h2 className="heading">Technologies</h2>
+    <div className="technologies">
+      <p className="col">HTML</p>
+      <p className="col">CSS</p>
+      <p className="col">Javascript</p>
+      <p className="col">ES6</p>
+      <p className="col">jQuery</p>
+      <p className="col">React</p>
+      <p className="col">Redux</p>
+      <p className="col">Node</p>
+      <p className="col">Express</p>
+      <p className="col">PostgreSQL</p>
+    </div>
+  </section>
+);
+
+export default tech;
