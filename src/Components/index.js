c7c006791108e34b0b67fa2009998e9500cefda5
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-25 17:09:45 -0500

    created base file for exporting all components

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+export { default as Burger } from "./Burger";
+export { default as Menu } from "./Menu";
+export { default as Header } from "./Header";
+export { default as Bio } from "./Bio";
+export { default as Tech } from "./Tech";
