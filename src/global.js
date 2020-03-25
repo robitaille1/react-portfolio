df019669e20cf1130ac4734fe7090ad0b174e69f
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-25 17:03:54 -0500

    added global styling for styled components

---
+++ src/global.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: ${({ theme }) => theme.primaryDark};
+    color: ${({ theme }) => theme.primaryLight};
+    /* height: 100vh; */
+    /* text-rendering: optimizeLegibility; */
+    /* font-family: Maven Pro, -apple-system,  BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"; */
+    font-family: 'Baloo Chettan 2', cursive;
+  }
+  div {
+    text-align: center;
+    /* @media (min-width: ${({ theme }) => theme.mobile}){
+      text-align: center;
+    } */
+  }
+  a {
+    color: ${({ theme }) => theme.primaryHover};
+    text-decoration: none;
+  }
+`;
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+// export const theme = {
+//   primaryDark: "#0D0C1D",
+//   primaryLight: "#EFFFFA",
+//   primaryHover: "#343078",
+//   mobile: "576px"
+// };
+
+export const theme = {
+  primaryDark: "#2b343f",
+  primaryLight: "white",
+  primaryHover: "#374458",
+  accent: "#2dbcad",
+  mobile: "576px"
+};
