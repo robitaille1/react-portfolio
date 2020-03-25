4ea6ff31f08ed02a2a30738cb7540c10db0491cb
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-25 17:06:20 -0500

    created basic styling and animation

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -1,18 +1,39 @@
 import React from "react";
-import "../Bio/Bio.css";
+import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
 const bio = () => (
-  <main className="bio">
-    <p>
-      I am a creative, problem solving web developer. I love working in teams
-      and contributing to projects that I feel passionate about. Creating
-      dynamic, responsive, and interactive web design is what I do best.
-    </p>
-    <p>
-      I also love sports (Go Chelsea!), tea, and traveling to experience
-      different cultures through food.
-    </p>
-  </main>
+  <Content>
+    <Fade up>
+      <h2>About Me</h2>
+      <p>
+        I am a creative, problem solving web developer. I love working in teams
+        and contributing to projects that I feel passionate about. Creating
+        dynamic, responsive, and interactive web design is what I do best.
+      </p>
+      <p>
+        I also love sports (Go Chelsea!), tea, and traveling to experience
+        different cultures through food.
+      </p>
+    </Fade>
+  </Content>
 );
 
 export default bio;
+
+const Content = styled.div`
+  padding: 0px 3em;
+  text-align: left;
+  @media (min-width: ${({ theme }) => theme.mobile}) {
+    padding: 0px 8em;
+  }
+  h2 {
+    font-size: 2.3rem;
+    color: ${({ theme }) => theme.accent};
+    margin: 0px;
+  }
+  p {
+    font-size: 1.5rem;
+    margin-top: 5px;
+  }
+`;
