8e1f6d7885fc7102c32d3da722e44141e3f799e9
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-29 12:07:45 -0500

    added svg icons for technologies - made responsive sctions

--- src/Components/Tech/Tech.js
+++ src/Components/Tech/Tech.js
@@ -1,21 +1,121 @@
 import React from "react";
+import styled from "styled-components";
+import reactLogo from "../../Assets/icons/React.svg";
+import htmlLogo from "../../Assets/icons/HTML5.svg";
+import cssLogo from "../../Assets/icons/CSS3.svg";
+import jsLogo from "../../Assets/icons/JS.svg";
+import es6Logo from "../../Assets/icons/ES6.svg";
+import reduxLogo from "../../Assets/icons/Redux.svg";
+import nodeLogo from "../../Assets/icons/Node.svg";
+import expressLogo from "../../Assets/icons/Express.svg";
+import mongoLogo from "../../Assets/icons/Mongo.svg";
+import postLogo from "../../Assets/icons/PostgreSQL.svg";
+import jqueryLogo from "../../Assets/icons/jQuery.svg";
 
 const tech = () => (
-  <section>
+  <Wrapper>
     <h2>Technologies</h2>
-    <div>
-      <p>HTML</p>
-      <p>CSS</p>
-      <p>Javascript</p>
-      <p>ES6</p>
-      <p>jQuery</p>
-      <p>React</p>
-      <p>Redux</p>
-      <p>Node</p>
-      <p>Express</p>
-      <p>PostgreSQL</p>
-    </div>
-  </section>
+
+    <Tech>
+      <div>
+        <h4>Frontend</h4>
+        <Frontend>
+          <img src={htmlLogo} alt="Html Logo" />
+          <img src={cssLogo} alt="CSS Logo" />
+          <img src={jsLogo} alt="Javascript Logo" />
+          <img src={es6Logo} alt="ES6 Logo" />
+          <img src={jqueryLogo} alt="jQuery Logo" />
+          <img src={reactLogo} alt="React Logo" />
+          <img src={reduxLogo} alt="Redux Logo" />
+        </Frontend>
+      </div>
+
+      <div>
+        <h4>Backend</h4>
+        <Backend>
+          <img src={nodeLogo} alt="Nodejs Logo" />
+          <img src={expressLogo} alt="Express Logo" />
+          <PostImg src={postLogo} alt="PostgreSQL Logo" />
+          <img src={mongoLogo} alt="Mongo Logo" />
+        </Backend>
+      </div>
+    </Tech>
+  </Wrapper>
 );
 
 export default tech;
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 1em 50px 1em;
+  background: lightgrey;
+  align-items: center;
+
+  @media (min-width: ${({ theme }) => theme.mobile}) {
+    padding: 50px 8em 100px 8em;
+  }
+
+  h2 {
+    font-size: 2.3rem;
+    width: 100%;
+    text-align: left;
+    color: ${({ theme }) => theme.primaryDark};
+  }
+  h4 {
+    margin: 20px auto;
+    text-align: center;
+    font-size: 30px;
+    color: ${({ theme }) => theme.primaryDark};
+  }
+`;
+
+const Tech = styled.div`
+  text-align: center;
+  border-radius: 5px;
+  @media (min-width: ${({ theme }) => theme.mobile}) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const Frontend = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  @media (min-width: ${({ theme }) => theme.mobile}) {
+    display: flex;
+    flex-direction: row;
+  }
+  img {
+    width: 75%;
+    margin: 20px auto;
+    @media (min-width: ${({ theme }) => theme.mobile}) {
+      width: 10%;
+      height: 10%;
+    }
+  }
+`;
+
+const Backend = styled.div`
+  display: grid;
+  padding-bottom: 20px;
+  @media (min-width: ${({ theme }) => theme.mobile}) {
+    display: flex;
+    flex-direction: row;
+  }
+  img {
+    width: 80%;
+    margin: 20px auto;
+    @media (min-width: ${({ theme }) => theme.mobile}) {
+      width: 10%;
+      /* height: 10%; */
+    }
+  }
+`;
+
+const PostImg = styled.img`
+  width: 40%;
+  @media (min-width: ${({ theme }) => theme.mobile}) {
+    width: 10%;
+  }
+`;
