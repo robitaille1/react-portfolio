a539d1b2855ff2b3b23cf69755c703c10d93b76b
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-31 14:52:02 -0500

    created base component with store object

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,111 +1,54 @@
 import React from "react";
-import "./Projects.scss";
+import styled from "styled-components";
+import Project from "./Project/Project";
+import puerhImg from "../../Assets/mockups/puerh-mockup.png";
+import hotImg from "../../Assets/mockups/hot-takes-mockup.png";
+import travelImg from "../../Assets/mockups/travel-mockup.png";
+
+const projectItm = [
+  {
+    title: "PUERH / CAFE",
+    description: "Organize puerh teas in your collection.",
+    link: "https://puerh-cafe.lucasrobitaille.now.sh/",
+    clientRepo: "https://github.com/robitaille1/puerh-cafe",
+    apiRepo: "https://github.com/robitaille1/puerh-cafe-api",
+    image: puerhImg,
+    tech: "React, Redux, Node, Express, PostgreSQL, and CSS."
+  },
+  {
+    title: "Hot Takes",
+    description:
+      "An app to keep track of all the 'hot takes' sports commentators have, in order to see who is the most reliable.",
+    link: "https://hot-takes-app.lucasrobitaille.now.sh/",
+    clientRepo: "https://github.com/robitaille1/hot-takes-app",
+    apiRepo: "https://github.com/robitaille1/hot-takes-api",
+    image: hotImg,
+    tech: "React, Redux, Node, Express, PostgreSQL, and CSS."
+  },
+  {
+    title: "Travel TV Search",
+    description: "Search for travel shows based on an input destination.",
+    link: "https://robitaille1.github.io/Travel-TV-Search/",
+    clientRepo: "https://github.com/robitaille1/Travel-TV-Search",
+    apiRepo: null,
+    image: travelImg,
+    tech: "Javascript, CSS, HTML, GoogleMaps Api, and TVMaze Api."
+  }
+];
 
 const projects = () => (
-  <section className="projects" id="Projects">
-    <h2 className="heading proj-head">Projects</h2>
-    <div>
-      <div className="project-item">
-        <p className="project-title">PUERH / CAFE</p>
-        <p className="app-desc">Organize puerh teas in your collection. </p>
-        <p className="app-desc">
-          Created with React, Redux, Node, Express, PostgreSQL, and CSS.
-        </p>
-        {/* <img
-          className="project-image"
-          src="assets/dash.PNG"
-          alt="PUERH / CAFE app"
-        /> */}
-        <a
-          href="https://github.com/robitaille1/puerh-cafe"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Client - Github
-        </a>
-        <a
-          href="https://github.com/robitaille1/puerh-cafe-api"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Api - Github
-        </a>
-        <a
-          href="https://puerh-cafe.lucasrobitaille.now.sh/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Live Link
-        </a>
-      </div>
-      <hr />
-      <div className="project-item">
-        <p className="project-title">Hot Takes</p>
-        <p className="app-desc">
-          An app to keep track of all the 'hot takes' sports commentators have,
-          in order to see who is the most reliable.
-        </p>
-        <p className="app-desc">
-          Created with React, Redux, Node, Express, PostgreSQL, and CSS.
-        </p>
-        {/* <img
-          className="project-image"
-          src="assets/HotTakes.PNG"
-          alt="Hot Takes App"
-        /> */}
-        <a
-          href="https://github.com/robitaille1/hot-takes-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Client - Github
-        </a>
-        <a
-          href="https://github.com/robitaille1/hot-takes-api"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Api - Github
-        </a>
-        <a
-          href="https://hot-takes-app.lucasrobitaille.now.sh/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Live Link
-        </a>
-      </div>
-      <hr />
-      <div className="project-item">
-        <p className="project-title">Travel TV Search</p>
-        <p className="app-desc">
-          Search for travel shows based on an input destination.
-        </p>
-        <p className="app-desc">
-          Created with Javascript, CSS, HTML, GoogleMaps Api, and TVMaze Api.
-        </p>
-        {/* <img
-          className="project-image"
-          src="assets/Travel_Home2.png"
-          alt="Travel TV Search App"
-        /> */}
-        <a
-          href="https://github.com/robitaille1/Travel-TV-Search"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-        <a
-          href="https://robitaille1.github.io/Travel-TV-Search/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Live Link
-        </a>
-      </div>
-    </div>
-  </section>
+  <Wrapper>
+    <h2>Projects</h2>
+  </Wrapper>
 );
 
 export default projects;
+
+const Wrapper = styled.section`
+  h2 {
+    font-size: 2.3rem;
+    width: 100%;
+    text-align: center;
+    color: ${({ theme }) => theme.primaryLight};
+  }
+`;
