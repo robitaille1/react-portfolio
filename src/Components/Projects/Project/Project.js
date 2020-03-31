6e7ec743682cc2ec2d96dc16b6de8600bc52161f
Lucas Robitaille <lucrobitaille@ymail.com>
2020-03-31 15:07:46 -0500

    added basic project card component

--- src/Components/Projects/Project/Project.js
+++ src/Components/Projects/Project/Project.js
@@ -1,8 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
+const project = props => (
+  <Card>
+    <div>
+      <h3>{props.data.title}</h3>
+      <p>{props.data.description}</p>
+      <p>Built with: {props.data.tech}</p>
+      <a href={props.data.link}>Live Link</a>
+      <a href={props.data.clientRepo}>Client Repo</a>
+      {props.data.apiRepo ? <a href={props.data.apiRepo}>Api Repo</a> : null}
+    </div>
+    <div>
+      <img src={props.data.image} alt={props.data.title + "image"} />
+    </div>
+  </Card>
+);
 
+export default project;
 
-
-
-const project = (props) => ();
+const Card = styled.div``;
