7c058f71e58518646eacb6a9e2b9d2909f21e023
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-02 14:09:00 -0500

    netlify form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,12 +6,7 @@ const contact = () => (
   <Wrapper name="Contact">
     <Social />
     <h2>Contact Me!</h2>
-    <ContactForm
-      name="contact"
-      method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
+    <ContactForm name="contact" method="POST">
       <input type="hidden" name="form-name" value="contact" />
       <label htmlFor="name">Name:</label>
       <input type="text" name="text" placeholder="John Smith" required />
