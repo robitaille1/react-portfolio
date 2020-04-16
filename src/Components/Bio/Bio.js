2c04da4f98735857123ced904f122a46015610ac
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-16 14:40:58 -0500

    added a picture for desktop

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -6,15 +6,24 @@ const bio = () => (
   <Content name="Bio">
     <Fade duration={2000}>
       <h2>About Me</h2>
-      <p>
-        I am a creative, problem solving web developer. I love working in teams
-        and contributing to projects that I feel passionate about. Creating
-        dynamic, responsive, and interactive web design is what I do best.
-      </p>
-      <p>
-        I also love sports (Go Chelsea!), tea, and traveling to experience
-        different cultures through food.
-      </p>
+      <div style={{ display: "flex" }}>
+        <Pic
+          src="https://media-exp1.licdn.com/dms/image/C4D03AQGA0HOg9Hc0yQ/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=Wx7_gH6zbLsH_YcAArWYiuqLt8Dt-vp7w6Ljkht9gOg"
+          alt="A Picture of Me!"
+        />
+        <div style={{ textAlign: "left" }}>
+          <p>
+            I am a creative, problem solving web developer. I love working in
+            teams and contributing to projects that I feel passionate about.
+            Creating dynamic, responsive, and interactive web design is what I
+            do best.
+          </p>
+          <p>
+            I also love sports (Go Chelsea!), tea, and traveling to experience
+            different cultures through food.
+          </p>
+        </div>
+      </div>
     </Fade>
   </Content>
 );
@@ -42,5 +51,16 @@ const Content = styled.div`
   p {
     font-size: 1.5rem;
     margin-top: 5px;
+  }
+`;
+
+const Pic = styled.img`
+  height: 50%;
+  margin-right: 40px;
+  margin-top: 10px;
+  display: none;
+
+  @media (min-width: 1000px) {
+    display: block;
   }
 `;
