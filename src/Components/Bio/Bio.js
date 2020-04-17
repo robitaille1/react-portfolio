0b98144daf58252aa0215a6a330d481b268a6591
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-17 12:27:03 -0500

    added filter drop shadow to parent div

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -3,29 +3,31 @@ import styled from "styled-components";
 import Fade from "react-reveal/Fade";
 
 const bio = () => (
-  <Content name="Bio">
-    <Fade duration={2000}>
-      <h2>About Me</h2>
-      <div style={{ display: "flex" }}>
-        <Pic
-          src="https://media-exp1.licdn.com/dms/image/C4D03AQGA0HOg9Hc0yQ/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=Wx7_gH6zbLsH_YcAArWYiuqLt8Dt-vp7w6Ljkht9gOg"
-          alt="A Picture of Me!"
-        />
-        <div style={{ textAlign: "left" }}>
-          <p>
-            I am a creative, problem solving web developer. I love working in
-            teams and contributing to projects that I feel passionate about.
-            Creating dynamic, responsive, and interactive web design is what I
-            do best.
-          </p>
-          <p>
-            I also love sports (Go Chelsea!), tea, and traveling to experience
-            different cultures through food.
-          </p>
+  <div style={{ filter: "drop-shadow(0px 10px 5px rgba(0,0,0,0.1))" }}>
+    <Content name="Bio">
+      <Fade duration={2000}>
+        <h2>About Me</h2>
+        <div style={{ display: "flex" }}>
+          <Pic
+            src="https://media-exp1.licdn.com/dms/image/C4D03AQGA0HOg9Hc0yQ/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=Wx7_gH6zbLsH_YcAArWYiuqLt8Dt-vp7w6Ljkht9gOg"
+            alt="A Picture of Me!"
+          />
+          <div style={{ textAlign: "left" }}>
+            <p>
+              I am a creative, problem solving web developer. I love working in
+              teams and contributing to projects that I feel passionate about.
+              Creating dynamic, responsive, and interactive web design is what I
+              do best.
+            </p>
+            <p>
+              I also love sports (Go Chelsea!), tea, and traveling to experience
+              different cultures through food.
+            </p>
+          </div>
         </div>
-      </div>
-    </Fade>
-  </Content>
+      </Fade>
+    </Content>
+  </div>
 );
 
 export default bio;
