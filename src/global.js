a8f7d762cf6d3bf509ed2045102b1198bd0a60c9
Lucas Robitaille <lucrobitaille@ymail.com>
2020-05-12 11:06:58 -0500

    made images non draggable

--- src/global.js
+++ src/global.js
@@ -30,4 +30,12 @@ export const GlobalStyles = createGlobalStyle`
     color: ${({ theme }) => theme.primaryHover};
     text-decoration: none;
   }
+  img {
+    user-drag: none;
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+  }
 `;
