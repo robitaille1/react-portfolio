aab326cf48801152fc6c66f13db25a3657c192b9
Lucas Robitaille <lucrobitaille@ymail.com>
2020-06-02 12:04:50 -0500

    removed unused code

--- src/Components/Tech/Tech.js
+++ src/Components/Tech/Tech.js
@@ -144,23 +144,18 @@ const Backend = styled.div`
   display: grid;
   padding-bottom: 20px;
   @media (min-width: ${({ theme }) => theme.mobile}) {
-    /* display: flex;
-    flex-direction: row;
-    flex-wrap: wrap; */
     grid-template-columns: 1fr 1fr;
   }
   img {
     width: 80%;
     margin: 20px auto;
     @media (min-width: ${({ theme }) => theme.mobile}) {
-      /* width: 20%; */
       margin: 30px auto;
     }
   }
   & :nth-child(3) {
     width: 40%;
     @media (min-width: ${({ theme }) => theme.mobile}) {
-      /* width: 20%; */
     }
   }
 `;
